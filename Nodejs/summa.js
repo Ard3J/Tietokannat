7eafe6887199a02b1d0f00f4c67da28b1f208bc2
Ard3J <arttujamsa@gmail.com>
2025-12-05 15:01:59 +0200

    18a hienosäätöä

--- Nodejs/summa.js
+++ Nodejs/summa.js
@@ -11,9 +11,9 @@ rl.question('Anna luku 1: ', input => {
         const luku2 = Number(input);
         rl.close();
 
-        if(luku1 >= luku2){
+        if(luku1 > luku2){
             console.log("Luku 1, "+luku1+" on suurempi");
-        } else if(luku2 >= luku1){
+        } else if(luku2 > luku1){
             console.log("Luku 2, "+luku2+" on suurempi");
         } else {
             console.log("Luvut ovat yhtä suuret");
